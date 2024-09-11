d4feb1cdbe5c9412854d8b9c5bf6e34b2b6f720d
Gifftybabe <ulokangozi@gmail.com>
2024-09-11 18:55:09 +0100

    config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
+const {ALCHEMY_URL} = process.env;
+const { PRIVATE_KEY } = process.env;
+const {ETHERSCAN_API} = process.env;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
-};
+  defaultNetwork: "sepolia",
+  solidity: "0.8.26",
+  networks: {
+    sepolia: {
+      url: ALCHEMY_URL, 
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API,
+     
+  },
+}; 
